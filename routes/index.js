bfb8b8a65ed724410d63186b71a94d7a5e314d44
KANG XI <kangxi@kx-iMac.local>
2018-01-12 00:31:47 +0800

    Add GTO

--- routes/index.js
+++ routes/index.js
@@ -88,6 +88,16 @@ router.get('/ticker/na', function (req, res) {
                 data.price = parseFloat(data.price_usd);
                 callback(null, data);
             });
+        },
+        DLT: function (callback) {
+            request.get({
+                url: "https://api.coinmarketcap.com/v1/ticker/agrello-delta/",
+                json: true
+            }, function (error, response, body) {
+                var data = body[0];
+                data.price = parseFloat(data.price_usd);
+                callback(null, data);
+            });
         }
     }, function (err, results) {
         res.json(results);
