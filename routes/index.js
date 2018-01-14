b4306851d85d43587377db7f8bbd7e05aed4fd2e
KANG XI <kangxi@kx-iMac.local>
2018-01-14 15:49:46 +0800

    Add yoyo and evx

--- routes/index.js
+++ routes/index.js
@@ -118,6 +118,26 @@ router.get('/ticker/na', function (req, res) {
         data.price = parseFloat(data.price_usd);
         callback(null, data);
       });
+    },
+    YOYO: function (callback) {
+      request.get({
+        url: "https://api.coinmarketcap.com/v1/ticker/yoyow/",
+        json: true
+      }, function (error, response, body) {
+        var data = body[0];
+        data.price = parseFloat(data.price_usd);
+        callback(null, data);
+      });
+    },
+    EVX: function (callback) {
+      request.get({
+        url: "https://api.coinmarketcap.com/v1/ticker/everex/",
+        json: true
+      }, function (error, response, body) {
+        var data = body[0];
+        data.price = parseFloat(data.price_usd);
+        callback(null, data);
+      });
     }
   }, function (err, results) {
     res.json(results);
